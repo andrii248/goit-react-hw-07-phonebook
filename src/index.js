638ae18363ddf8d6b13f4aa5c67a47f925d073c3
Andrii Krupa <krpaandrii@gmail.com>
2022-08-26 08:19:09 +0300

    Added ToastContainer into the main index.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
+import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import {
   store,
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer />
     </Provider>
   </React.StrictMode>
 );
